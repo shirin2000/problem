b0b6792b13712a6222abeced01ef8fadcb45b9ed
shirin2000 <59328792+shirin2000@users.noreply.github.com>
2024-01-21 15:19:48 +0530

    Update forms.py

--- app1/forms.py
+++ app1/forms.py
@@ -1,15 +1,23 @@
 from django import forms
 from .models import Problems
 from .models import MyProject
+from .models import Blog
+
 
 class MyProjectForm(forms.ModelForm):
     class Meta:
         model = MyProject
-        fields = '__all__'
+        fields = ['solution', 'current_status', 'works_left']
+        exclude = ['problem']
 
 
 class ProblemForm(forms.ModelForm):
     class Meta:
         model = Problems
         fields = '__all__'
+
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['heading', 'content', 'created_by']
 
