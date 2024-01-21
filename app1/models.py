40ebd42c54fcc579ac9afe88e8bbe65894b12fab
shirin2000 <59328792+shirin2000@users.noreply.github.com>
2024-01-21 15:20:25 +0530

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -10,8 +10,22 @@ class Problems(models.Model):
         return f"{self.sector} - {self.problem_definition[:50]}" 
 
 class MyProject(models.Model):
+    sector = models.CharField(max_length=20)
     problem = models.TextField()  # Corresponding values of the "Problem" column
     solution = models.TextField()
     current_status = models.CharField(max_length=100)
     works_left = models.CharField(max_length=100)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Blog(models.Model):
+    heading = models.CharField(max_length=255)
+    content = models.TextField()
+    created_by = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def short_content(self):
+        return self.content[:100] + ('...' if len(self.content) > 100 else '')
+
+    def __str__(self):
+        return self.heading
